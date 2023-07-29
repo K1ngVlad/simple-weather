893f1d1e11dc19c144a7a9d17090de5c26add6fe
K1ngVlad <kingvlad3008@gmail.com>
2023-07-29 20:52:06 +0400

    test

--- api/getRegion.tsx
+++ api/getRegion.tsx
@@ -2,6 +2,7 @@ import { Geo } from '@/interfaces';
 
 const getRegion = async (q: string, apiKey: string) => {
   const url = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${q}`;
+  console.log(url);
   const response = await fetch(url);
   const data: Geo[] = await response.json();
   if (data.length) {
