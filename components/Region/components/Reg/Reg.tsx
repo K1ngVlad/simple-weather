bba73f21e8d9dbdd94571b39ffc60469d035aecc
K1ngVlad <kingvlad3008@gmail.com>
2023-07-29 20:47:06 +0400

    test

--- components/Region/components/Reg/Reg.tsx
+++ components/Region/components/Reg/Reg.tsx
@@ -14,6 +14,8 @@ const Reg: FC<props> = ({ apiKey }) => {
   const { nav, setNav } = useNav();
   const { lat, lon, loading, region } = nav;
 
+  console.log(apiKey);
+
   useEffect(() => {
     if (window?.navigator?.geolocation && apiKey) {
       navigator.geolocation.getCurrentPosition(async ({ coords }) => {
