f243bd61b100a5c120ae3fa369f10e20093b3d00
K1ngVlad <kingvlad3008@gmail.com>
2023-07-29 21:11:32 +0400

    nav error

--- components/Region/components/Reg/Reg.tsx
+++ components/Region/components/Reg/Reg.tsx
@@ -16,22 +16,29 @@ const Reg: FC<props> = ({ apiKey }) => {
 
   useEffect(() => {
     if (window?.navigator?.geolocation && apiKey) {
-      navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-        const { latitude, longitude } = coords;
-        const q = `${latitude},${longitude}`;
-        const currentRegion = await getRegion(q, apiKey);
-        if (currentRegion) {
-          setNav((e) => ({ ...e, region: currentRegion, loading: false }));
-        } else {
+      navigator.geolocation.getCurrentPosition(
+        async ({ coords }) => {
+          const { latitude, longitude } = coords;
+          const q = `${latitude},${longitude}`;
+          const currentRegion = await getRegion(q, apiKey);
+          if (currentRegion) {
+            setNav((e) => ({ ...e, region: currentRegion, loading: false }));
+          } else {
+            setNav((e) => ({ ...e, loading: false }));
+          }
+        },
+        () => {
           setNav((e) => ({ ...e, loading: false }));
         }
-      });
+      );
     } else {
       setNav((e) => ({ ...e, loading: false }));
     }
   }, []);
 
-  return <h3 className={s.regionChange}>{`${loading ? 'Москва' : region}`}</h3>;
+  return (
+    <h3 className={s.regionChange}>{`${loading ? 'Подождите...' : region}`}</h3>
+  );
 };
 
 export { Reg };
